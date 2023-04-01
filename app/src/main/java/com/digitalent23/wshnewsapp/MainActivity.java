854e579de5b262a9d241783d8cf5498e2a9670bd
Inno <inokisheo@gmail.com>
2023-04-01 12:21:59 +0300

    first build

---
+++ app/src/main/java/com/digitalent23/wshnewsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.digitalent23.wshnewsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
